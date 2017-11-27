08aa430ec4aa64675f37c0c6a86cecb8fdfb69e1
luffyliang@163.com <0634zhang>
2017-11-27 13:30:30 +0800

    添加controller。

---
+++ src/main/java/com/wxsell/luffy/Controller/CategoryController.java
@@ -0,0 +1,31 @@
+package com.wxsell.luffy.Controller;
+
+import com.wxsell.luffy.Service.CategoryService;
+import com.wxsell.luffy.pojo.product.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+/**
+ * @Author:luffyhongliang
+ * @Date:Created in :16:33 2017/11/23
+ * @Description:
+ **/
+@RestController
+@RequestMapping("/seller")
+public class CategoryController {
+     @Autowired
+    CategoryService categoryService;
+
+    @RequestMapping(value="/{id}",method= RequestMethod.GET)
+    public String list(@PathVariable("id") Integer id, Map<String,Object> map){
+        Category category =categoryService.findOne(id);
+        map.put("name",category.getCategoryName());
+        return "category";
+    }
+
+}
